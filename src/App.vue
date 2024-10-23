bc08fd29ccaa9a2a126a4376344b355608019f4e
Abdulloh <R.abdulloh04@icloud.com>
2024-10-23 12:01:57 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="bg-[url('../public/unsplash_fn--TuQvBZ0.png')]">
+    <div class="container">
+      <div>
+        <h1 class="font-bold text-[72px] leading-[92px] text-white max-w-[639px] w-full mb-[24px]">Go beyond and create your space</h1>
+        <p  class="font-normal text-[22px] leading-[32px] text-white max-w-[450px] w-full">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed idt.</p>
+        <button class="font-normal text-[20px] text-black bg-white mb-[145px] px-[40px] py-[10px] mt-[24px] rounded-[40px]">Get started</button>
+      </div>
+      <div class="mt-10 gap-[15px] flex">
+      <div class="bg-[#242424] p-5 rounded-lg mx-2">
+        <img class="mb-[16px]" src="../public/Icons.svg" alt="" />
+        <span class="text-white mb-[16px]">Title</span>
+        <p class="text-[15px] max-w-[266px] w-full  text-white  opacity-[60%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum.</p>
+      </div>
+      <div class="bg-[#242424] p-5 rounded-lg mx-2">
+        <img class="mb-[16px]" src="../public/Icons.svg" alt="" />
+        <span class="text-white mb-[16px]">Title</span>
+        <p class="text-[15px] max-w-[266px] w-full  text-white  opacity-[60%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum.</p>
+      </div>
+      <div class="bg-[#242424] p-5 rounded-lg mx-2">
+        <img class="mb-[16px]" src="../public/Icons.svg" alt="" />
+        <span class="text-white mb-[16px]">Title</span>
+        <p class="text-[15px] max-w-[266px] w-full  text-white  opacity-[60%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum.</p>
+      </div>
+      <div class="bg-[#242424] p-5 rounded-lg mx-2">
+        <img class="mb-[16px]" src="../public/Icons.svg" alt="" />
+        <span class="text-white mb-[16px]">Title</span>
+        <p class="text-[15px] max-w-[266px] w-full  text-white  opacity-[60%]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cursus imperdiet sed id elementum.</p>
+      </div>
+      
+    </div>
+    </div>
+  </section>
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx,vue}',],
+  theme: {
+    extend: {},
+    container: {
+      padding: "50px",
+    }
+  },
+ 
+  plugins: [],
+}
+
